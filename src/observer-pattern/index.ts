867d897c9c1ff86b37df141f29616e07e53e262d
Mehran <janfeshan.mehran@gmail.com>
2021-07-13 11:31:43 +0200

    Adding observer-pattern

---
+++ src/observer-pattern/index.ts
@@ -0,0 +1,120 @@
+// The Observer is a design pattern where an object (known as a subject)
+// maintains a list of objects depending on it (observers), automatically notifying them of any changes to state.
+
+function ObserverList() {
+  this.observerList = []
+}
+
+ObserverList.prototype.add = function (obj) {
+  return this.observerList.push(obj)
+}
+
+ObserverList.prototype.count = function () {
+  return this.observerList.length
+}
+
+ObserverList.prototype.get = function (index) {
+  if (index > -1 && index < this.observerList.length) {
+    return this.observerList[index]
+  }
+}
+
+ObserverList.prototype.indexOf = function (obj, startIndex) {
+  let i = startIndex
+
+  while (i < this.observerList.length) {
+    if (this.observerList[i] === obj) {
+      return i
+    }
+    i++
+  }
+
+  return -1
+}
+
+ObserverList.prototype.removeAt = function (index) {
+  this.observerList.splice(index, 1)
+}
+
+
+// Subject
+function Subject() {
+  this.observers = new ObserverList()
+}
+
+Subject.prototype.addObserver = function (observer) {
+  this.observers.add(observer)
+}
+
+Subject.prototype.removeObserver = function (observer) {
+  this.observers.removeAt(this.observers.indexOf(observer, 0))
+}
+
+Subject.prototype.notify = function (context) {
+  const observerCount = this.observers.count()
+  for (let i = 0; i < observerCount; i++) {
+    this.observers.get(i).update(context)
+  }
+}
+
+
+// The Observer
+function Observer() {
+  this.update = function () {
+    // ...
+  }
+}
+
+
+// Extend an object with an extension
+function extend(obj, extension) {
+  for (const key in extension) {
+    obj[key] = extension[key]
+  }
+}
+
+// References to our DOM elements
+
+const controlCheckbox = document.getElementById('mainCheckbox')
+const addBtn = document.getElementById('addNewObserver')
+const container = document.getElementById('observersContainer')
+
+
+// Concrete Subject
+
+// Extend the controlling checkbox with the Subject class
+extend(controlCheckbox, new Subject())
+
+// Clicking the checkbox will trigger notifications to its observers
+controlCheckbox.onclick = function () {
+  // @ts-ignore
+  controlCheckbox.notify(controlCheckbox.checked)
+}
+
+addBtn.onclick = addNewObserver
+
+// Concrete Observer
+
+function addNewObserver() {
+
+  // Create a new checkbox to be added
+  const check = document.createElement('input')
+  check.type = 'checkbox'
+
+  // Extend the checkbox with the Observer class
+  extend(check, new Observer())
+
+  // Override with custom update behaviour
+  // @ts-ignore
+  check.update = function (value) {
+    this.checked = value
+  }
+
+  // Add the new observer to our list of observers
+  // for our main subject
+  // @ts-ignore
+  controlCheckbox.addObserver(check)
+
+  // Append the item to the container
+  container.appendChild(check)
+}
---
+++ src/observer-pattern/pub-sub.ts
@@ -0,0 +1,108 @@
+import { inflate } from 'zlib'
+
+const pubsub = {
+  subscribe: undefined,
+  publish: undefined,
+  unsubscribe: undefined
+};
+
+(function (myObject) {
+
+  // Storage for topics that can be broadcast
+  // or listened to
+  const topics = {}
+
+  // A topic identifier
+  let subUid = -1
+
+  // Publish or broadcast events of interest
+  // with a specific topic name and arguments
+  // such as the data to pass along
+  myObject.publish = function (topic, args) {
+
+    if (!topics[topic]) {
+      return false
+    }
+
+    const subscribers = topics[topic]
+    let len = subscribers ? subscribers.length : 0
+
+    while (len--) {
+      subscribers[len].func(topic, args)
+    }
+
+    return this
+  }
+
+  // Subscribe to events of interest
+  // with a specific topic name and a
+  // callback function, to be executed
+  // when the topic/event is observed
+  myObject.subscribe = function (topic, func) {
+
+    if (!topics[topic]) {
+      topics[topic] = []
+    }
+
+    const token = (++subUid).toString()
+    topics[topic].push({
+      token: token,
+      func: func
+    })
+    return token
+  }
+
+  // Unsubscribe from a specific
+  // topic, based on a tokenized reference
+  // to the subscription
+  myObject.unsubscribe = function (token) {
+    for (const m in topics) {
+      if (topics[m]) {
+        for (var i = 0, j = topics[m].length; i < j; i++) {
+          if (topics[m][i].token === token) {
+            topics[m].splice(i, 1)
+            return token
+          }
+        }
+      }
+    }
+    return this
+  }
+}(pubsub))
+
+
+// Another simple message handler
+
+// A simple message logger that logs any topics and data received through our
+// subscriber
+const messageLogger = function (topics, data) {
+  console.log('Logging: ' + topics + ': ' + data)
+}
+
+// Subscribers listen for topics they have subscribed to and
+// invoke a callback function (e.g. messageLogger) once a new
+// notification is broadcast on that topic
+const subscription = pubsub.subscribe('inbox/newMessage', messageLogger)
+
+// Publishers are in charge of publishing topics or notifications of
+// interest to the application. e.g:
+
+pubsub.publish('inbox/newMessage', 'hello world!')
+
+// or
+pubsub.publish('inbox/newMessage', ['test', 'a', 'b', 'c'])
+
+// or
+pubsub.publish('inbox/newMessage', {
+  sender: 'hello@google.com',
+  body: 'Hey again!'
+})
+
+// We can also unsubscribe if we no longer wish for our subscribers
+// to be notified
+pubsub.unsubscribe(subscription)
+
+// Once unsubscribed, this for example won't result in our
+// messageLogger being executed as the subscriber is
+// no longer listening
+pubsub.publish('inbox/newMessage', 'Hello! are you still there?')
